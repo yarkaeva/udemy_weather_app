0a093e01b9146dccd5f2f7f797e59b314a6d6b84
yarkaeva <darina.yarkaeva@gmail.com>
2022-04-12 01:24:52 +0500

    master

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,10 @@ Widget _buildBody() {
                 icon: Icon(Icons.search, color: Colors.white)))
         ),
         _cityDetail(),
-         
+        _temperatureDetail(),
+        _extraWeatherDetail(),
+       Text('7-day weather forecast'.toUpperCase(), style: const TextStyle(color: Colors.white, fontSize: 20.0)),
+       BodyListView()
       ],
     ),
   );
@@ -53,14 +56,100 @@ Column _cityDetail () {
         'Tyumen Oblast, RU',
         style: TextStyle(
           color: Colors.white,
-          fontSize: 30.0)
+          fontSize: 35.0)
           ),
       Text(
         'Monday, Apr 11, 2022',
         style: TextStyle(
           color: Colors.white,
-          fontSize: 18.0)
+          fontSize: 16.0)
           )
+    ],
+  );
+}
+
+Row _temperatureDetail(){
+  return Row(
+     mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      const Icon(Icons.wb_sunny, color: Colors.white, size: 80,),
+      const SizedBox(width: 18.0),
+      Column(
+        children: [
+          const Text(
+            '14°F',
+            style: TextStyle(
+            color: Colors.white,
+            fontSize: 40.0)),
+          Text(
+            'Light snow'.toUpperCase(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 16.0),)
+        ],
+      )
+    ],
+  );
+}
+
+Row _extraWeatherDetail() {
+  return Row(
+     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Column(
+        children: const[
+          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+          Text('5', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          Text('km/hr', style: TextStyle(color: Colors.white))
+          ],
+      ),
+      Column(
+        children: const[
+          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+          Text('3', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          Text('%', style: TextStyle(color: Colors.white))
+          ],
+      ),
+       Column(
+        children: const[
+          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+          Text('20', style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          Text('%', style: TextStyle(color: Colors.white))
+          ],
+      ),
+    ],
+  );
+}
+
+class  BodyListView extends StatelessWidget {
+  const  BodyListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
+}
+
+Widget _myListView() {
+  return ListView(
+    padding: const EdgeInsets.all(8),
+    scrollDirection: Axis.horizontal,
+    itemExtent: 350,
+    children:  [
+      Container(
+        width: 300,
+        height: 250,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Monday',style: TextStyle(color: Colors.white, fontSize: 25.0)),
+            Row(children: const [
+               Text('6°F', style: TextStyle(color: Colors.white, fontSize: 35.0)),
+              Icon(Icons.ac_unit,color: Colors.white,size: 35)
+            ],)
+          ],
+        ),
+      )
     ],
   );
 }
