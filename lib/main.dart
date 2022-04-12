d3a505cdb94cf30df0aeb1d024d3de6a37bec469
yarkaeva <darina.yarkaeva@gmail.com>
2022-04-12 14:36:24 +0500

    Add paddings between widgets and add Listview.

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.red, 
-        primaryColor: Colors.white),
+          scaffoldBackgroundColor: Colors.red, primaryColor: Colors.white),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Weather Forecast',
-              style: TextStyle(color: Colors.white)
-              ),
+              style: TextStyle(color: Colors.white)),
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
@@ -26,66 +24,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 Widget _buildBody() {
-  return SingleChildScrollView(
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children:  [
-        const TextField(
-            decoration: (InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Enter City name',
-                hintStyle: TextStyle(color: Colors.white),
-                icon: Icon(Icons.search, color: Colors.white)))
-        ),
-        _cityDetail(),
-        _temperatureDetail(),
-        _extraWeatherDetail(),
-       Text('7-day weather forecast'.toUpperCase(), style: const TextStyle(color: Colors.white, fontSize: 20.0)),
-       BodyListView()
-      ],
-    ),
-  );
-}
-
-Column _cityDetail () {
   return Column(
-    children: const[
-      Text(
-        'Tyumen Oblast, RU',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 35.0)
-          ),
-      Text(
-        'Monday, Apr 11, 2022',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 16.0)
-          )
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      const TextField(
+          style: TextStyle(color: Colors.white),
+          decoration: (InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter City name',
+              hintStyle: TextStyle(color: Colors.white),
+              icon: Icon(Icons.search, color: Colors.white)))),
+      Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _cityDetail(),
+            _temperatureDetail(),
+            _extraWeatherDetail(),
+            _bottomDetail()
+          ],
+        ),
+      )
     ],
   );
 }
 
-Row _temperatureDetail(){
+Column _cityDetail() {
+  return Column(
+    children: const [
+      Text('Tyumen Oblast, RU',
+          style: TextStyle(color: Colors.white, fontSize: 35.0)),
+      Text('Monday, Apr 11, 2022',
+          style: TextStyle(color: Colors.white, fontSize: 16.0))
+    ],
+  );
+}
+
+Row _temperatureDetail() {
   return Row(
-     mainAxisAlignment: MainAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      const Icon(Icons.wb_sunny, color: Colors.white, size: 80,),
+      const Icon(
+        Icons.wb_sunny,
+        color: Colors.white,
+        size: 80,
+      ),
       const SizedBox(width: 18.0),
       Column(
         children: [
-          const Text(
-            '14°F',
-            style: TextStyle(
-            color: Colors.white,
-            fontSize: 40.0)),
+          const Text('14°F',
+              style: TextStyle(color: Colors.white, fontSize: 40.0)),
           Text(
             'Light snow'.toUpperCase(),
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 16.0),)
+            style: const TextStyle(color: Colors.white, fontSize: 16.0),
+          )
         ],
       )
     ],
@@ -94,35 +88,45 @@ Row _temperatureDetail(){
 
 Row _extraWeatherDetail() {
   return Row(
-     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     children: [
       Column(
-        children: const[
-          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+        children: const [
+          Icon(Icons.ac_unit, color: Colors.white, size: 30),
           Text('5', style: TextStyle(color: Colors.white, fontSize: 20.0)),
           Text('km/hr', style: TextStyle(color: Colors.white))
-          ],
+        ],
       ),
       Column(
-        children: const[
-          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+        children: const [
+          Icon(Icons.ac_unit, color: Colors.white, size: 30),
           Text('3', style: TextStyle(color: Colors.white, fontSize: 20.0)),
           Text('%', style: TextStyle(color: Colors.white))
-          ],
+        ],
       ),
-       Column(
-        children: const[
-          Icon(Icons.ac_unit, color: Colors.white,size: 30 ), 
+      Column(
+        children: const [
+          Icon(Icons.ac_unit, color: Colors.white, size: 30),
           Text('20', style: TextStyle(color: Colors.white, fontSize: 20.0)),
           Text('%', style: TextStyle(color: Colors.white))
-          ],
+        ],
       ),
     ],
   );
 }
 
-class  BodyListView extends StatelessWidget {
-  const  BodyListView({Key? key}) : super(key: key);
+Column _bottomDetail() {
+  return Column(
+    children: [
+      Text('7-day weather forecast'.toUpperCase(),
+          style: const TextStyle(color: Colors.white, fontSize: 18.0)),
+      const BodyListView()
+    ],
+  );
+}
+
+class BodyListView extends StatelessWidget {
+  const BodyListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -131,25 +135,75 @@ class  BodyListView extends StatelessWidget {
 }
 
 Widget _myListView() {
-  return ListView(
-    padding: const EdgeInsets.all(8),
-    scrollDirection: Axis.horizontal,
-    itemExtent: 350,
-    children:  [
-      Container(
-        width: 300,
-        height: 250,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text('Monday',style: TextStyle(color: Colors.white, fontSize: 25.0)),
-            Row(children: const [
-               Text('6°F', style: TextStyle(color: Colors.white, fontSize: 35.0)),
-              Icon(Icons.ac_unit,color: Colors.white,size: 35)
-            ],)
-          ],
+  return Container(
+    height: 140,
+    padding: const EdgeInsets.all(16),
+    child: ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        Container(
+          width: 160,
+          color: Colors.red[200],
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text('Monday',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('6°F',
+                      style: TextStyle(color: Colors.white, fontSize: 35.0)),
+                  Icon(Icons.wb_sunny, color: Colors.white, size: 35)
+                ],
+              ),
+            ],
+          ),
         ),
-      )
-    ],
+        const SizedBox(width: 8),
+        Container(
+          width: 160,
+          color: Colors.red[200],
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text('Tuesday',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('13°F',
+                      style: TextStyle(color: Colors.white, fontSize: 35.0)),
+                  Icon(Icons.wb_sunny, color: Colors.white, size: 35)
+                ],
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 8),
+        Container(
+          width: 160,
+          color: Colors.red[200],
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const Text('Wednesday',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('11°F',
+                      style: TextStyle(color: Colors.white, fontSize: 35.0)),
+                  Icon(Icons.wb_sunny, color: Colors.white, size: 35)
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
   );
 }
